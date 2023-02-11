57757ad43b2da2bda866fe9ca111724d166d4a65
SBZ systems <support@sbzsystems.com>
2023-02-11 10:06:11 +0200

    Delete mysql_whmcs_clients_with_no_active_services.sql

--- mysql_whmcs_clients_with_no_active_services.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM tblclients
-WHERE
-    
-(select count( tblhosting.id) from tblhosting where tblclients.id=tblhosting.userid and tblhosting.domainstatus='Active')=0
-and  
-(select count( tbldomains.id) from tbldomains where tblclients.id=tbldomains.userid and tbldomains.status='Active')=0  
